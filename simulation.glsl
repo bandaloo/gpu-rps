655bd8334bb7dee7f80fcde5895c1f161469c602
bandaloo <bandaloo@icloud.com>
2020-02-17 21:12:46 -0500

    removed old commented out code

--- simulation.glsl
+++ simulation.glsl
@@ -50,18 +50,4 @@ void main() {
   else if (i < 6.0) {munch(center, 1, - 1); }
   else if (i < 7.0) {munch(center, 1, 0); }
   else if (i < 8.0) {munch(center, 1, 1); }
-  
-  /*
-  if (sum == 3) {
-    // ideal # of neighbors... if cell is living, stay alive, if it is dead, come to life!
-    gl_FragColor = vec4(1.0);
-  } else if (sum == 2) {
-    // maintain current state
-    float current = float(get(0, 0));
-    gl_FragColor = vec4(vec3(current), 1.0);
-  } else {
-    // over-population or lonliness... cell dies
-    gl_FragColor = vec4(vec3(0.0), 1.0);
-  }
-  */
 }
